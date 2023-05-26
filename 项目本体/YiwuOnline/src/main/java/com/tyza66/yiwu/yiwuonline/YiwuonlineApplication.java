37e5517d533c1de91441455511c5f4c027646a6d
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-26 09:27:45 +0800

    update

---
+++ 项目本体/YiwuOnline/src/main/java/com/tyza66/yiwu/yiwuonline/YiwuonlineApplication.java
@@ -0,0 +1,13 @@
+package com.tyza66.yiwu.yiwuonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YiwuonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiwuonlineApplication.class, args);
+    }
+
+}
---
+++ 项目本体/YiwuOnline/src/test/java/com/tyza66/yiwu/yiwuonline/YiwuonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyza66.yiwu.yiwuonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YiwuonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
