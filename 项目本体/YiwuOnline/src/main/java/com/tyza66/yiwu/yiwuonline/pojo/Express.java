36ffe84e7b4fe51cc2eb55ed5e25d368d62f262b
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-29 15:52:11 +0800

    update

---
+++ 项目本体/YiwuOnline/src/main/java/com/tyza66/yiwu/yiwuonline/pojo/Express.java
@@ -0,0 +1,20 @@
+package com.tyza66.yiwu.yiwuonline.pojo;
+
+import lombok.Data;
+
+/**
+ * Author: tyza66
+ * Date: 2023/05/29 15:49
+ * Github: https://github.com/tyza66
+ **/
+@Data
+public class Express {
+    Integer id;
+    String name;
+    String location;
+    String express;
+    String phone;
+    String destination;
+    String status;
+    String trackingNumber;
+}
