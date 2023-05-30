1d455b0df50255e84125cd71042541aa9f2b9393
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-05-30 16:46:15 +0800

    update

---
+++ 数据库表/20230530/yiwu.sql
@@ -0,0 +1,112 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80033
+ Source Host           : localhost:3306
+ Source Schema         : yiwu
+
+ Target Server Type    : MySQL
+ Target Server Version : 80033
+ File Encoding         : 65001
+
+ Date: 30/05/2023 16:45:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for express
+-- ----------------------------
+DROP TABLE IF EXISTS `express`;
+CREATE TABLE `express`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `express` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `destination` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `tracking_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `num` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of express
+-- ----------------------------
+INSERT INTO `express` VALUES (1, '自口海锅1002', '村头', '中通快递', '123456789', '村尾', '41654165456456485', '已送达', 10);
+INSERT INTO `express` VALUES (4, '旺日王大礼包1', '黑市1', '11', '1915621', '11', NULL, '', 11);
+INSERT INTO `express` VALUES (5, '旺日王大礼包111', '黑市111', '1111', '191562111', '1111', NULL, '没了1', 1111);
+
+-- ----------------------------
+-- Table structure for inventory
+-- ----------------------------
+DROP TABLE IF EXISTS `inventory`;
+CREATE TABLE `inventory`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `from1` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `band` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `bandpeople` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `price` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of inventory
+-- ----------------------------
+INSERT INTO `inventory` VALUES (1, '旺日王大礼包', '临期山寨旺旺大礼包', '黑市', '旺妄', '神秘人', '11234596', '10元', '100包');
+INSERT INTO `inventory` VALUES (2, '雷碧', '山寨雪碧', '超市', '可口渴了', '小王', '564156456', '1元', '100箱');
+INSERT INTO `inventory` VALUES (3, '哇哈哈', '假娃哈哈', '村头', '哇哈哈', '二狗', '53245455', '0.5元', '20箱');
+INSERT INTO `inventory` VALUES (6, '日王仔QQ糖', '山寨版旺仔QQ糖', 'QQ', '汪仔', '老刘', '1915621', '0.5元', '11包');
+
+-- ----------------------------
+-- Table structure for outbound
+-- ----------------------------
+DROP TABLE IF EXISTS `outbound`;
+CREATE TABLE `outbound`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `express` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `destination` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `num` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of outbound
+-- ----------------------------
+INSERT INTO `outbound` VALUES (1, '雪碧', '村头', '中通快递', '15909805566', '村尾', 10);
+INSERT INTO `outbound` VALUES (6, '旺日王大礼包1', '黑市1', '11', '1915621', '11', 11);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ban` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `top_user_id` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`, `username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2055708674 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('123', '123', '4456', 'false', 1);
+INSERT INTO `user` VALUES ('456', '456', '789', 'false', 1);
+INSERT INTO `user` VALUES ('LL', 'LL', 'll', 'false', 1);
+INSERT INTO `user` VALUES ('tyza66', 'tyza66', 'tyza66', 'false', 1);
+INSERT INTO `user` VALUES ('tyza666', 'tyza666', 'tyza66', 'false', 1);
+
+SET FOREIGN_KEY_CHECKS = 1;
